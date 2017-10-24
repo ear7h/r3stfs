02dc6d53da75ac69ff32f48844385efb74f4316b
Julio <julio.grillo98@gmail.com>
2017-10-23 20:29:27 -0700

    added go file for go get will refactor everything to github.com/ear7h/r3stfs/*

---
+++ r3stfs.go
@@ -0,0 +1,11 @@
+/*
+this is will eventually have the command line tool
+for now it is a placeholder go file for the go get command
+ */
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("resty")
+}
