18da10412cdc74aa9f714dd01d2e7b74815dfa0b
Julio <julio.grillo98@gmail.com>
2018-03-25 22:40:32 -0700

    new error type for asserting an error is caused by the user

--- server/errors.go
+++ server/errors.go
@@ -16,3 +16,25 @@ func IsNotImplemented(e error) bool {
 	_, ok := e.(*NotImplementedError)
 	return ok
 }
+
+// User error, corresponds with a bad request
+type UserError struct {
+	msg string
+}
+
+func (e *UserError) Error() string {
+	return e.msg
+}
+
+func NewUserError(msg string) *UserError {
+	return &UserError{msg:msg}
+}
+
+func WrapUserError(e error) *UserError {
+	return &UserError{msg:e.Error()}
+}
+
+func IsUser(e error) bool {
+	_, ok := e.(*UserError)
+	return ok
+}
