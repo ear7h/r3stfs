76a4936802f6b6de00067301e8443072d2fb4d2b
Julio <julio.grillo98@gmail.com>
2018-03-25 22:46:21 -0700

    meber function descriptions

--- server/fs_handler.go
+++ server/fs_handler.go
@@ -8,9 +8,14 @@ import (
 // FsHandler is an interface which defines a serving abstraction over the filesystem
 // the file names at this point are assumed to be safe
 type FsHandler interface {
+	// Read files
 	HandleGet(header http.Header, filename string) (io.ReadCloser, error)
-	HandlePut(header http.Header, filename string, body io.ReadCloser) (int, error)
-	HandlePost(header http.Header, filename string, body io.ReadCloser) (int, error)
+	// Create or write files
+	HandlePut(header http.Header, filename string, body io.Reader) (int, error)
+	// Exclusive create
+	HandlePost(header http.Header, filename string, body io.Reader) (int, error)
+	// Delete files
 	HandleDelete(header http.Header, filename string) (error)
+	// Check available methods for file
 	HandleOptions(header http.Header, filename string) (io.ReadCloser, error)
 }
